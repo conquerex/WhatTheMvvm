2a805c82df415e44192912503bfb76da4b1d9653
Jongkook <conquerex@gmail.com>
2021-02-19 06:56:42 +0900

    Add BaseKotlinActivity

--- app/src/main/java/what/the/mvvm/MainActivity.kt
+++ app/src/main/java/what/the/mvvm/MainActivity.kt
@@ -1,11 +1,28 @@
 package what.the.mvvm
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import what.the.mvvm.base.BaseKotlinActivity
+import what.the.mvvm.databinding.ActivityMainBinding
+import what.the.mvvm.viewmodel.MainViewModel
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : BaseKotlinActivity<ActivityMainBinding, MainViewModel>() {
+    override val layoutResourceId: Int
+        get() = R.layout.activity_main
+
+    // todo : 아래 주석은 블로그 포스팅 후 제거 예정
+    // Expression 'viewModel' of type 'MainViewModel' cannot be invoked as a function. The function 'invoke()' is not found
+    // 해결책 : import org.koin.androidx.viewmodel.ext.android.viewModel
+    override val viewModel: MainViewModel by viewModel()
+
+    override fun initStartView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initDataBinding() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initAfterBinding() {
+        TODO("Not yet implemented")
     }
 }
