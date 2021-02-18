add5556074098a724631fbf0da1341b7cebb79f2
Jongkook <conquerex@gmail.com>
2021-02-19 06:26:34 +0900

    Koin setting

--- app/src/main/java/what/the/mvvm/MyApplication.kt
+++ app/src/main/java/what/the/mvvm/MyApplication.kt
@@ -2,6 +2,7 @@ package what.the.mvvm
 
 import android.app.Application
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import what.the.mvvm.di.myDiModule
 
@@ -11,8 +12,15 @@ import what.the.mvvm.di.myDiModule
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        // 모듈 등록
         startKoin {
+            // Koin이 로그를 남기는 레벨을 지정 - Level.INFO by default
+            androidLogger()
+            // Koin은 코틀린 환경이라면 어디든 동작한다
+            // 코틀린 환경의 안드로이드 앱 개발시 androidContext로 Context를 주입시킨다
+            // inject Android context
             androidContext(this@MyApplication)
+            // 선언한 모듈 지정
             modules(myDiModule)
         }
     }
--- app/src/main/java/what/the/mvvm/di/MyModule.kt
+++ app/src/main/java/what/the/mvvm/di/MyModule.kt
@@ -7,6 +7,14 @@ import what.the.mvvm.viewmodel.MainViewModel
 /**
  * Created by jongkook on 2021.02.17
  */
+
+/**
+ * 모듈 선언 후 변수 저장 방법
+ * single  : 앱이 실행되는 동안 계속 유지되는 싱글톤 객체를 생성합니다.
+ * factory : 요청할 때마다 매번 새로운 객체를 생성합니다.
+ * get()   : 컴포넌트 내에서 알맞은 의존성을 주입 받습니다.
+ * ViewModel의 경우 viewModel 키워드로 선언
+ */
 var viewModelPart = module {
     viewModel {
         MainViewModel()
