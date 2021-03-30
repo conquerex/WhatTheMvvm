b74007596ce45fd24405f16dcf9e3a8f8ee5bbe0
Barley <conquerex@gmail.com>
2021-03-30 11:20:52 +0900

    Add Practice Activity

---
+++ app/src/main/java/what/the/mvvm/practice/PracticeActivity.kt
@@ -0,0 +1,11 @@
+package what.the.mvvm.practice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PracticeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_practice)
+    }
+}
